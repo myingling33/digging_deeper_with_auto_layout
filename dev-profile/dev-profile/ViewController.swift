1c761d303d18bf5df383d7e224b7b9d35d7bbaa9
Michael David Yingling <michaelyingling@Michaels-MacBook-Pro.local>
2018-09-04 06:27:41 -0500

    initial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Michael David Yingling on 9/4/18.
+//  Copyright © 2018 Michael David Yingling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
